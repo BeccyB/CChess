48761862bbbd5337366f1fd0a0f9d79eea084952
AntonReiswich <anton.reiswichmail@gmail.com>
2022-11-10 21:17:52 +0100

    changed name of test

--- tests/test_make_move.cc
+++ tests/test_make_move.cc
@@ -3,7 +3,7 @@
 #include <gtest/gtest.h>
 
 
-TEST(HelloTest, BasicAssertions) {
+TEST(TestQueen, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
