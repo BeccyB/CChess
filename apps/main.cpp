25913b092817796c0a1e508aae5978bf6214af55
AntonReiswich <anton.reiswichmail@gmail.com>
2025-06-17 18:26:50 +0200

    setup cmake

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,60 +1,8 @@
 #include <iostream>
-#include <memory>
-
-#include <chess/game.h>
-
-void show_options() {
-    // Game options
-}
-
-// TODO(rebecca) move this method to chess library
-void main_menu() {
-    std::cout << "Chess game menu:" << std::endl;
-    std::cout << "Enter \"S\" to start new Game." << std::endl;
-    std::cout << "Enter \"O\" for Options" << std::endl;
-    std::cout << "Enter \"E\" for Exit" << std::endl;
-    std::cout << "Enter \"H\" for Help." << std::endl;
-
-    char selection;
-
-    std::cin >> selection;
-
-    if (selection == 'S') {
-
-        std::string name_white;
-        std::cout << "Enter player white:" << std::endl;
-        std::cin >> name_white;
-
-        std::string name_black;
-        std::cout << "Enter player black:" << std::endl;
-        std::cin >> name_black;
-
-        // start game:
-        auto game = std::make_unique<chess::Game>();
-        game->start();
-        return;
-    }
-
-    if (selection == 'O') {
-        show_options(); // TODO...
-        return;
-    }
-
-    if (selection == 'H') {
-        std::cout << "Help is coming..."
-                  << std::endl; // How to operate our game ...
-        std::cout << "If you want to learn more about chess, read wikipedia: "
-                     "https://en.wikipedia.org/wiki/Chess. "
-                  << std::endl;
-        return;
-    }
-
-    if (selection == 'E') {
-        return;
-    }
-}
 
 int main() {
 
-    main_menu();
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+
 }
--- tests/test_game.cc
+++ tests/test_game.cc
@@ -1,7 +1,16 @@
-#include <chess/game.h>
-
 #include <gtest/gtest.h>
 
 TEST(TestGame, BasicAssertions) {
-    // TODO add tests for game
+
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+    // Expect that boolean is true.
+    EXPECT_TRUE(true);
+    // Expect that a pointer is not null.
+    int* ptr = new int(5);
+    EXPECT_NE(ptr, nullptr);
+    delete ptr;  // Clean up the allocated memory.
+
 }
