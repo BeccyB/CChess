9db8f40d596328e8d13b7888f0a4abe13f02cce7
Rebecca <rebecca.elisabeth@outlook.com>
2025-08-26 19:05:51 +0200

    added cmake gcov

---
+++ cmake/gcov.cmake
@@ -0,0 +1,35 @@
+OPTION(TIGL_COVERAGE_GENHTML "Use Genhtml to generate htmls from gcov output" OFF)
+
+FIND_PROGRAM(GCOV_PATH gcov)
+
+IF(NOT GCOV_PATH)
+    MESSAGE(FATAL_ERROR "gcov not found! Aborting...")
+ENDIF()
+
+IF(NOT CMAKE_COMPILER_IS_GNUCXX AND NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    MESSAGE(FATAL_ERROR "Compiler is not GNU gcc or clang! Aborting...")
+ENDIF()
+
+# Would only seting O0
+IF(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
+    MESSAGE(WARNING "Code coverage results with an optimised (non-Debug) build may be misleading")
+ENDIF()
+
+# Setup compiler options
+IF(CMAKE_COMPILER_IS_GNUCXX)
+    ADD_DEFINITIONS(-fprofile-arcs -ftest-coverage)
+    LINK_LIBRARIES(gcov)
+ENDIF()
+
+function(setup_code_coverage _targetname _testrunner _outputname)
+    IF(NOT GCOVR_PATH)
+        MESSAGE(FATAL_ERROR "gcovr not found! Aborting...")
+    ENDIF() # NOT GCOVR_PATH
+
+    ADD_CUSTOM_TARGET(${_targetname}
+        # Run tests
+        COMMAND ${_testrunner} ${ARGV3}
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/tests/unittests
+        COMMENT "Running coverage tests."
+    )
+endfunction()
