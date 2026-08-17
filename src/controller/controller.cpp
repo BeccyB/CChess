38916022ace86f39df54f221c8e80c2fa938b285
AntonReiswich <anton.reiswichmail@gmail.com>
2026-08-17 09:05:08 +0200

    forgot to save updates; now commit again

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -9,11 +9,6 @@ void Controller::run_game() {
 
     while (_board.pawn_reached_other_side() == false) {
         _viewer.display_game(_board);
-        
-        // if (_viewer.ask_user_if_pawn_should_go_one_step_forward()) {
-        //     // _board.move_pawn_one_step_forward();
-        //     count_steps ++;
-        // }
 
         // add a bit of control flow here:
         // _viewer.ask_user_to_select_a_pawn_that_will_go_one_step_forward(); // shall return x,y coordinate
@@ -23,13 +18,9 @@ void Controller::run_game() {
         // if(check if there is a pawn on this position == false)
             // cout: no pawn on this position. Enter new coordinates, with pawn. // add loop here
         //move_pawn_one_step_forward();         // board gets updated
-        //if(check if pawn reached other side() == true)
-            // cout: game over. Pawn reached other side
-
 
         break;
-
     }
-    
+
     _viewer.print_pawn_reached_other_side_and_game_over();
 }
--- src/view/abstract_user_interface.h
+++ src/view/abstract_user_interface.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 #include "src/model/chessgame.h"
 
 class Viewer {
@@ -8,7 +10,7 @@ public:
 
     // bool ask_user_if_pawn_should_go_one_step_forward() const;
 
-    bool ask_user_to_select_a_pawn_that_shall_go_one_step_forward() const;
+    std::pair<int, int> ask_user_to_select_a_pawn_that_shall_go_one_step_forward() const;
 
     void print_pawn_reached_other_side_and_game_over() const;
 };
